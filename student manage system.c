9191b687674dfe8292e925ee82b80f98bb3f0728
Yuan.Sn <81820474+AwesomeOps@users.noreply.github.com>
2024-06-18 23:39:53 +0800

    Add files via upload

--- student manage system.c
+++ student manage system.c
@@ -13,9 +13,10 @@ struct student{
 	char std_nmb[STD_NMBSIZE];
 	int ch_score;
 	int mt_score;
-	int en_score;	
+	int en_score;
+	int sum_score;	
 }menbers[MENBERS];
-int count=0; //记录输入数据数量 
+int count=0;	//记录输入数据数量 
 void prtstar(int a){	//打印 *号 
 	int i;
 	for(i=0;i<a;i++)
@@ -26,11 +27,42 @@ void prtspace(int a){	//打印' '字符
 	for(i=0;i<a;i++)
 	printf(" ");
 }; 
-void menu(void); //菜单函数 
-void scandate(void); //输入数据 
-void printdate(void); //打印数据 
-int sum_score(char *ch_score,char *mt_score,char *en_score); //求总成绩 
+void menu(void);	//菜单函数 
+void scandate(void);	//输入数据 
+void printdate(void);	//打印数据 
+void rank(void);	//排序学生成绩 
+void search_score(void){
+	system("cls");
+	char name[20];
+	int i;
+	printf("请输入需要查询学生的名字：\n");
+	scanf("%s",&name);
+	for(i=0;i<count;i++){
+		if((strcmp(name,menbers[i].name))==0){
+			printf("%s的语文成绩为：%d，数学成绩为：%d，英语成绩为：%d",menbers[i].name,menbers[i].ch_score,menbers[i].mt_score,menbers[i].en_score);
+		}	
+	}
+}
+void scandate_flie(void){	//扫描txt内的数据 
+	FILE *fp;
+	int i;
+	fp=fopen("date.txt","r+");
+	fscanf(fp,"%d\n",&count);
+	for(i=0;i<count;i++)
+	fscanf(fp,"%s %s %s %d %d %d %d\n",menbers[i].name,menbers[i].sex,menbers[i].std_nmb,&menbers[i].ch_score,&menbers[i].mt_score,&menbers[i].en_score,&menbers[i].sum_score); 
+	fclose(fp);
+} 
+void print_flie(void){	//将结构体的数据提取到txt 
+	FILE *fp;
+	int i;
+	fp=fopen("date.txt","w+");
+	fprintf(fp,"%d\n",count);
+	for(i=0;i<count;i++)
+	fprintf(fp,"%s %s %s %d %d %d %d\n",menbers[i].name,menbers[i].sex,menbers[i].std_nmb,menbers[i].ch_score,menbers[i].mt_score,menbers[i].en_score,menbers[i].sum_score);
+	fclose(fp);
+} 
 int main(){
+	scandate_flie();
 	while(1){
 	if(count>0){
 		putchar('\n');
@@ -42,7 +74,16 @@ int main(){
 		case'1':scandate();
 				break;
 		case'2':printdate();
-				break;	
+				break;
+		case'3':rank();
+				break;
+		case'4':search_score();
+				break;
+		case'0':
+				printf("Bye Bye!\n");
+				exit(0);
+				break;
+					
 		}		
 	}	
 	return 0;
@@ -56,20 +97,22 @@ void menu(void){
 	putchar('\n');
 	printf("*             1.输入学生信息            *\n");
 	printf("*             2.打印学生成绩            *\n");
-	printf("*             3.排序学生成绩            *\n");
-	printf("*             4.计算成绩均值            *\n");
+	printf("*             3.根据成绩排名            *\n");
+	printf("*             4.查询学生成绩            *\n");
 	printf("*             5.计算最高成绩            *\n");
 	printf("*             6.计算最低成绩            *\n");
+	printf("*             0.退出程序                *\n");
 	prtstar(41);
 	printf("\n");
 }
 void scandate(void){ 
 	char choose;
 	do{
-	if(choose>0){
+	if(count>0){
 		prtstar(21);
 		putchar('\n');
 	}
+	scandate_flie();
 	puts("请输入学生姓名:"); 
 	gets(menbers[count].name);
 	puts("请输入学生性别:"); 
@@ -82,7 +125,9 @@ void scandate(void){
 	scanf("%d",&menbers[count].mt_score);
 	puts("请输入英语成绩:");
 	scanf("%d",&menbers[count].en_score);
+	menbers[count].sum_score=menbers[count].ch_score+menbers[count].mt_score+menbers[count].en_score;
 	count++;
+	print_flie();
 	puts("是否要继续输入（y/n):");
 	getchar();
 	choose=_getch();
@@ -95,5 +140,18 @@ void printdate(void){
 	printf("%s\t%s\t%s\t%d\t%d\t%d\t%d\n"
 	,menbers[i].name,menbers[i].sex,menbers[i].std_nmb,menbers[i].ch_score,menbers[i].mt_score,menbers[i].en_score
 	,menbers[i].ch_score+menbers[i].mt_score+menbers[i].en_score);
+}
+void rank(){	//排序学生总成绩 
+	int i,j;
+	struct student change;
+	for(i=0;i<count;i++)
+		for(j=0;j<count-i;j++){
+			if(menbers[i+1].sum_score>menbers[i].sum_score){
+				change=menbers[i];
+				menbers[i]=menbers[i+1];
+				menbers[i+1]=change;
+			}
+		}
+	printdate();
 }
 
