650f39bd2e3620b42e814b6e1a5d290afe2aec90
Yuan.Sn <81820474+AwesomeOps@users.noreply.github.com>
2024-06-19 19:56:34 +0800

    Add files via upload

--- student manage system.c
+++ student manage system.c
@@ -15,7 +15,11 @@ struct student{
 	int mt_score;
 	int en_score;
 	int sum_score;	
-}menbers[MENBERS];
+};
+struct student menbers[MENBERS];
+struct student ex[MENBERS];
+struct student fi[MENBERS];
+struct student nm[MENBERS];
 int count=0;	//记录输入数据数量 
 void prtstar(int a){	//打印 *号 
 	int i;
@@ -34,22 +38,35 @@ void rank(void);	//排序学生成绩
 void search_score(void);	//根据名字查询分数 
 void scandate_flie(void);	//扫描txt内的数据 
 void print_flie(void);		//将txt数据传到结构体 
-//void analyze(void){
-//	int i; 
-//	printf("优秀\t良好\t一般\t差"); 
-//	for(i=0;i<count;i++){
-//		if(menbers[i].sum_score>360)
-//			printf("\n%s",menbers[i].name);
-//		if(menbers[i].sum_score>160)
-//			printf("\t%s",menbers[i].name);
-//		if(menbers[i].sum_score>140)
-//			printf("\t\t%s",menbers[i].name);
-//		if(menbers[i].sum_score>120)
-//			printf("\t\t%s",menbers[i].name);
-//			
-//	}
-//}
- 
+void analyze(void);		//分析学生成绩 
+void change_stu(void){
+	scandate_flie();
+	int i;
+	int a;
+	printf("   姓名\t性别\t学号\t\t语文\t数学\t英语\t总分\n"); 
+	for (i=0;i<count;i++)
+	printf("%d. %s\t%s\t%s\t%d\t%d\t%d\t%d\n"
+	,i+1,menbers[i].name,menbers[i].sex,menbers[i].std_nmb,menbers[i].ch_score,menbers[i].mt_score,menbers[i].en_score
+	,menbers[i].ch_score+menbers[i].mt_score+menbers[i].en_score);	
+	puts("请输入要修改学生的序号：");
+	scanf("%d",&a);
+	getchar();
+	puts("请输入学生姓名:"); 
+	gets(menbers[a-1].name);
+	puts("请输入学生性别:"); 
+	gets(menbers[a-1].sex);
+	puts("请输入学生学号:"); 
+	gets(menbers[a-1].std_nmb);
+	puts("请输入语文成绩:");
+	scanf("%d",&menbers[a-1].ch_score);
+	puts("请输入数学成绩:");
+	scanf("%d",&menbers[a-1].mt_score);
+	puts("请输入英语成绩:");
+	scanf("%d",&menbers[a-1].en_score);
+	menbers[a-1].sum_score=menbers[a-1].ch_score+menbers[a-1].mt_score+menbers[a-1].en_score;
+	
+} 
+
 int main(){
 	scandate_flie();
 	while(1){
@@ -70,6 +87,8 @@ int main(){
 				break;
 		case'5':analyze();
 				break;
+		case'6':change_stu();
+				break;		
 		case'0':
 				printf("Bye Bye!\n");
 				exit(0);
@@ -79,7 +98,7 @@ int main(){
 	}	
 	return 0;
 }
-void menu(void){
+void menu(void){ 
 	prtstar(41);
 	putchar('\n');
 	printf("*\t欢迎使用学生成绩管理系统\t*\n");
@@ -175,4 +194,110 @@ void search_score(void){	//根据名字查询分数
 		}	
 	}
 }
+void analyze(void){
+	int i; 
+	char ch;
+	print_flie();
+	int count_ex=0;
+	int count_fi=0;
+	int count_nm=0;
+	int count_fl=0;
+	struct student ex[MENBERS];
+	struct student fi[MENBERS];
+	struct student nm[MENBERS];
+	struct student fl[MENBERS];
+	for(i=0;i<count;i++){
+		if(menbers[i].sum_score>270){
+			strcpy(ex[count_ex].name,menbers[i].name);
+			strcpy(ex[count_ex].sex,menbers[i].sex);
+			strcpy(ex[count_ex].std_nmb,menbers[i].std_nmb);
+			ex[count_ex].ch_score=menbers[i].ch_score;
+			ex[count_ex].mt_score=menbers[i].mt_score;
+			ex[count_ex].en_score=menbers[i].en_score;
+			ex[count_ex].sum_score=menbers[i].sum_score;
+			count_ex++;
+			continue;
+		}
+		if(menbers[i].sum_score>240){
+			strcpy(fi[count_fi].name,menbers[i].name);
+			strcpy(fi[count_fi].sex,menbers[i].sex);
+			strcpy(fi[count_fi].std_nmb,menbers[i].std_nmb);
+			fi[count_fi].ch_score=menbers[i].ch_score;
+			fi[count_fi].mt_score=menbers[i].mt_score;
+			fi[count_fi].en_score=menbers[i].en_score;
+			fi[count_fi].sum_score=menbers[i].sum_score;
+			count_fi++;
+			continue;
+		}
+		if(menbers[i].sum_score>180){
+			strcpy(nm[count_nm].name,menbers[i].name);
+			strcpy(nm[count_nm].sex,menbers[i].sex);
+			strcpy(nm[count_nm].std_nmb,menbers[i].std_nmb);
+			nm[count_nm].ch_score=menbers[i].ch_score;
+			nm[count_nm].mt_score=menbers[i].mt_score;
+			nm[count_nm].en_score=menbers[i].en_score;
+			nm[count_nm].sum_score=menbers[i].sum_score;
+			count_nm++;
+			continue;
+		}
+		if(menbers[i].sum_score<180){
+			strcpy(fl[count_fl].name,menbers[i].name);
+			strcpy(fl[count_fl].sex,menbers[i].sex);
+			strcpy(fl[count_fl].std_nmb,menbers[i].std_nmb);
+			fl[count_fl].ch_score=menbers[i].ch_score;
+			fl[count_fl].mt_score=menbers[i].mt_score;
+			fl[count_fl].en_score=menbers[i].en_score;
+			fl[count_fl].sum_score=menbers[i].sum_score;
+			count_fl++;
+			continue;
+		}		
+	} 
+	printf("1.打印成绩优秀学生 2.打印成绩良好学生 3.打印成绩一般学生 4.打印不及格学生\n");
+	ch=_getch();
+	switch(ch){
+		case '1':
+			system("cls");
+			for(i=0;i<count_ex;i++)
+			printf("%s\t%s\t%s\t%d\t%d\t%d\t%d\n"
+			,ex[i].name,ex[i].sex,menbers[i].std_nmb,ex[i].ch_score,ex[i].mt_score,ex[i].en_score
+			,ex[i].sum_score);
+			printf("(本次考试共有%d人参考，优秀的共有%d人)",count,count_ex);
+			getchar();
+			system("cls");
+			break;
+		case '2':
+			system("cls");
+			for(i=0;i<count_fi;i++)
+			printf("%s\t%s\t%s\t%d\t%d\t%d\t%d\n"
+			,fi[i].name,fi[i].sex,menbers[i].std_nmb,fi[i].ch_score,fi[i].mt_score,fi[i].en_score
+			,fi[i].sum_score);
+			printf("(本次考试共有%d人参考，良好的共有%d人)",count,count_fi);
+			getchar();
+			system("cls");
+			break;
+		case '3':
+			system("cls");
+			for(i=0;i<count_nm;i++)
+			printf("%s\t%s\t%s\t%d\t%d\t%d\t%d\n"
+			,nm[i].name,nm[i].sex,menbers[i].std_nmb,nm[i].ch_score,nm[i].mt_score,nm[i].en_score
+			,nm[i].sum_score);
+			printf("(本次考试共有%d人参考，一般的共有%d人)",count,count_nm);
+			getchar();
+			system("cls");
+			break;
+		case '4':
+			system("cls");
+			for(i=0;i<count_nm;i++)
+			printf("%s\t%s\t%s\t%d\t%d\t%d\t%d\n"
+			,fl[i].name,fl[i].sex,menbers[i].std_nmb,fl[i].ch_score,fl[i].mt_score,fl[i].en_score
+			,fl[i].sum_score);
+			printf("(本次考试共有%d人参考，不及格的共有%d人)",count,count_fl);
+			getchar();
+			system("cls");
+			break;
+		case '0':
+			exit(0);
+	}
+}
+ 
 
